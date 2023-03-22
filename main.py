4674b6a4665836e63baa9c914aac98cc6699f2d9
Alterhalo <125083637+Alterhalo@users.noreply.github.com>
2023-03-22 18:56:32 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,146 +1,115 @@
-knife = False
-sword = False
-magic = False
-shield = False
+import random
 
-def diningRoom():
-  actions = ["fight","flee"]
-  global knife
-  global shield
-  print("A strange man with an AK47 appears. You can either run or fight him. What would you like to do?")
-  userInput = ""
-  while userInput not in actions:
-    print("Options: flee/fight")
-    userInput = input()
-    if userInput == "fight":
-      if shield:
-        print("You block the bullets with the shield you found earlier. You throw your knife and kill him.")
-        cameraScene()
-      else:
-        print("The creature has killed you.")
-      quit()
-    elif userInput == "flee":
-      cameraScene()
-    else:
-      print("Please enter a valid option.")
-    
-    
-def strangeCreature():
-  actions = ["fight","flee"]
-  global knife
-  global shield
-  print("A strange goul-like creature has appeared. You can either run or fight it. What would you like to do?")
-  userInput = ""
-  while userInput not in actions:
-    print("Options: flee/fight")
-    userInput = input()
-    if userInput == "fight":
-      if knife:
-        print("You kill the goul with the knife you found earlier. You found a shield!")
-        shield = True
-        showSkeletons()
-      else:
-        print("The goul-like creature has killed you.")
-      quit()
-    elif userInput == "flee":
-      showSkeletons()
-    else:
-      print("Please enter a valid option.")
-      
-def showSkeletons():
-  directions = ["backward","forward"]
-  global knife
-  print("You see a wall of skeletons as you walk into the room. Someone is watching you. Where would you like to go?")
-  userInput = ""
-  while userInput not in directions:
-    print("Options: left/backward/forward")
-    userInput = input()
-    if userInput == "left":
-      print("You find that this door opens into a wall. You open some of the drywall to discover a knife.")
-      knife = True
-    elif userInput == "backward":
-      introScene()
-    elif userInput == "forward":
-      strangeCreature()
-    else:
-      print("Please enter a valid option.")
-      
+# Define a class for the player's movement
+class Movement:
+    def __init__(self, direction):
+        self.direction = direction
 
-def hauntedRoom():
-  directions = ["right","left","backward"]
-  print("You hear strange voices. You think you have awoken some of the dead. Where would you like to go?")
-  userInput = ""
-  while userInput not in directions:
-    print("Options: right/left/backward")
-    userInput = input()
-    if userInput == "right":
-      print("Multiple goul-like creatures start emerging as you enter the room. You are killed.")
-      quit()
-    elif userInput == "left":
-      print("You made it! You've found an exit.")
-      quit()
-    elif userInput == "backward":
-      introScene()
-    else:
-      print("Please enter a valid option.")
-
-def cameraScene():
-  directions = ["forward","backward"]
-  print("You see a camera that has been dropped on the ground. Someone has been here recently. Where would you like to go?")
-  userInput = ""
-  while userInput not in directions:
-    print("Options: forward/backward")
-    userInput = input()
-    if userInput == "forward":
-      print("You see a strange looking room.")
-      diningRoom()
-    elif userInput == "backward":
-      showShadowFigure()
-    else:
-      print("Please enter a valid option.")
-      
-def showShadowFigure():
-  directions = ["right","backward"]
-  print("You see a dark shadowy figure appear in the distance. You are creeped out. Where would you like to go?")
-  userInput = ""
-  while userInput not in directions:
-    print("Options: right/left/backward")
-    userInput = input()
-    if userInput == "right":
-      cameraScene()
-    elif userInput == "left":
-      print("You find that this door opens into a wall.")
-    elif userInput == "backward":
-      introScene()
-    else:
-      print("Please enter a valid option.")
+    def go(self):
+        if self.direction == "north":
+            print("You head north.")
+        elif self.direction == "south":
+            print("You head south.")
+        elif self.direction == "east":
+            print("You head east.")
+        elif self.direction == "west":
+            print("You head west.")
+        else:
+            print("Invalid direction.")
 
 
-def introScene():
-  directions = ["left","right","forward"]
-  print("You are at a crossroads, and you can choose to go down any of the four hallways. Where would you like to go?")
-  userInput = ""
-  while userInput not in directions:
-    print("Options: left/right/backward/forward")
-    userInput = input()
-    if userInput == "left":
-      showShadowFigure()
-    elif userInput == "right":
-      showSkeletons()
-    elif userInput == "forward":
-      hauntedRoom()
-    elif userInput == "backward":
-      print("You find that this door opens into a wall.")
-    else: 
-      print("Please enter a valid option.")
+# Define a class for the player's inventory
+class Inventory:
+    def __init__(self):
+        self.items = []
 
-if __name__ == "__main__":
-  while True:
-    print("Welcome to MEGA HORROR")
-    print("As an avid traveller, you have decided to visit the Catacombs of Vatican City \nYou are also looking for you long lost father!.")
-    print("However, during your exploration, you find yourself lost.")
-    print("You can choose to walk in multiple directions to find a way out.")
-    print("Let's start with your name: ")
-    name = input()
-    print("Good luck, " +name+ ".")
-    introScene()
+    def add_item(self, item):
+        self.items.append(item)
+        print(f"{item} added to your inventory.")
+
+    def remove_item(self, item):
+        if item in self.items:
+            self.items.remove(item)
+            print(f"{item} removed from your inventory.")
+        else:
+            print(f"{item} not found in your inventory.")
+
+    def view_inventory(self):
+        if len(self.items) == 0:
+            print("Your inventory is empty.")
+        else:
+            print("Inventory:")
+            for item in self.items:
+                print(item)
+
+
+# Define a class for the enemy
+class Enemy:
+    def __init__(self, name, health, damage):
+        self.name = name
+        self.health = health
+        self.damage = damage
+
+    def attack(self):
+        return random.randint(1, self.damage)
+
+    def take_damage(self, damage):
+        self.health -= damage
+        if self.health <= 0:
+            print(f"{self.name} has been defeated.")
+            return True
+        else:
+            print(f"{self.name} has {self.health} health remaining.")
+            return False
+
+
+# Define the game loop
+def game_loop():
+    # Create the player's inventory and set their starting location
+    inventory = Inventory()
+    location = "forest"
+
+    # Create the enemy
+    enemy = Enemy("Goblin", 10, 3)
+
+    # Loop until the player either defeats the enemy or dies
+    while True:
+        # Display the player's options and get their choice
+        print(f"You are in the {location}.")
+        print("Options:")
+        print("1. Move")
+        print("2. View inventory")
+        print("3. Attack enemy")
+        print("4. Quit game")
+
+        choice = input("Enter your choice: ")
+
+        # Process the player's choice
+        if choice == "1":
+            # Allow the player to move in different directions
+            direction = input("Enter a direction (north, south, east, west): ")
+            movement = Movement(direction)
+            movement.go()
+            location = "cave"
+        elif choice == "2":
+            # View the player's inventory
+            inventory.view_inventory()
+        elif choice == "3":
+            # Combat with the enemy
+            print("You attack the Goblin.")
+            enemy.take_damage(random.randint(1, 5))
+            if enemy.take_damage(random.randint(1, 5)):
+                print("You have defeated the Goblin.")
+                break
+            print(f"The Goblin attacks you for {enemy.attack()} damage.")
+        elif choice == "4":
+            # Quit the game
+            print("Thanks for playing!")
+            break
+        else:
+            # Invalid choice
+            print("Invalid choice.")
+
+
+# Start the game
+game_loop()
